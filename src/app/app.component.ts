cd513dae5449fea17ff4dd9f3911b167d0d4d579
Catgirl <zeroaninea@gmail.com>
2025-08-26 00:49:54 +0400

    app-hero imported

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 // import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { HeroComponent } from './hero/hero.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, HeroComponent],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
