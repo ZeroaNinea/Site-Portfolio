40c0f7cb0816392187bcfade9eadcce26407e34b
Catgirl <zeroaninea@gmail.com>
2025-08-26 00:47:16 +0400

    HeroComponent created

---
+++ src/app/hero/hero.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero',
+  imports: [],
+  standalone: true,
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.scss',
+})
+export class HeroComponent {}
