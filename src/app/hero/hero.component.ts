f3573cc9d0e280e4a6fd1a48b6e4173ea5bb1304
Catgirl <zeroaninea@gmail.com>
2025-08-26 11:03:08 +0400

    the arrow icon fixed

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [MatIconModule],
   standalone: true,
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss',
