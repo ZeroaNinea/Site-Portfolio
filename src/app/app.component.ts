3d2bf3363e1482622054a3b90158a9c6573fff8b
Catgirl <zeroaninea@gmail.com>
2025-08-27 13:54:22 +0400

    about added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 // import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { HeroComponent } from './hero/hero.component';
+import { AboutComponent } from './about/about.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, HeroComponent],
+  imports: [HeaderComponent, HeroComponent, AboutComponent],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
