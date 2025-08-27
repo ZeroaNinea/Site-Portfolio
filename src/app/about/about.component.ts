a7c986cb379a64bfa9dd34a51f8fe4fa21f659fe
Catgirl <zeroaninea@gmail.com>
2025-08-27 19:03:10 +0400

    the link to the author added

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   imports: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   standalone: true,
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
