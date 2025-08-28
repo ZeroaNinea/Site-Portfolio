949b3037fedd3f0b0ae78312e381f822dea66c30
Catgirl <zeroaninea@gmail.com>
2025-08-28 19:11:47 +0400

    the theme changed to dark manually

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, Renderer2 } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 // import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { HeroComponent } from './hero/hero.component';
@@ -13,4 +14,15 @@ import { AboutComponent } from './about/about.component';
 })
 export class AppComponent {
   title = 'portfolio';
+
+  private document = inject(DOCUMENT);
+  private renderer = inject(Renderer2);
+
+  constructor() {
+    const html = this.document.querySelector('html');
+    const about = this.document.querySelector('#about');
+
+    this.renderer.addClass(html, 'dark-theme');
+    this.renderer.removeClass(html, 'light-theme');
+  }
 }
