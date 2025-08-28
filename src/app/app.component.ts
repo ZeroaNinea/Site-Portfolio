c83bdb13fd2cdb14b1c39dd36c07bfae01837fd7
Catgirl <zeroaninea@gmail.com>
2025-08-28 23:08:57 +0400

    animated with TS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import {
   Renderer2,
   DOCUMENT,
   afterEveryRender,
+  afterNextRender,
 } from '@angular/core';
 
 // import { RouterOutlet } from '@angular/router';
@@ -26,39 +27,75 @@ export class AppComponent {
   private observer: IntersectionObserver | undefined;
 
   constructor() {
-    afterEveryRender(() => {
-      const html = this.document.querySelector('html');
-      const about = this.document.querySelector('#about');
-      const aboutSection = this.document.querySelector('section.about');
+    // afterNextRender(() => {
+    const html = this.document.querySelector('html');
+    const about = this.document.querySelector('#about');
+    const aboutSection = this.document.querySelector('section.about');
 
-      if (about) {
-        this.observer = new IntersectionObserver(
-          (entries) => {
-            entries.forEach((entry) => {
-              if (entry.isIntersecting) {
-                this.renderer.addClass(html, 'dark-theme');
-                this.renderer.removeClass(html, 'light-theme');
+    if (about) {
+      this.observer = new IntersectionObserver(
+        (entries) => {
+          entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+              this.renderer.addClass(html, 'dark-theme');
+              this.renderer.removeClass(html, 'light-theme');
 
-                console.log(aboutSection);
-                this.renderer.addClass(aboutSection, 'appear-form-left');
-                console.log(aboutSection);
-              } else {
-                this.renderer.addClass(html, 'light-theme');
-                this.renderer.removeClass(html, 'dark-theme');
+              aboutSection?.animate(
+                [
+                  {
+                    transform: 'translateX(-200px)',
+                    opacity: 0,
+                    filter: 'blur(1px)',
+                  },
+                  {
+                    transform: 'translateX(50px)',
+                    opacity: 0.5,
+                  },
+                  {
+                    transform: 'translateX(0)',
+                    filter: 'blur(0px)',
+                    opacity: 1,
+                  },
+                ],
+                {
+                  duration: 1000,
+                }
+              );
+            } else {
+              this.renderer.addClass(html, 'light-theme');
+              this.renderer.removeClass(html, 'dark-theme');
 
-                console.log(aboutSection);
-                this.renderer.removeClass(aboutSection, 'appear-form-left');
-                console.log(aboutSection);
-              }
-            });
-          },
-          {
-            threshold: 0.5,
-          }
-        );
+              aboutSection?.animate(
+                [
+                  {
+                    transform: 'translateX(-50px)',
+                    opacity: 0.5,
+                  },
+                  {
+                    transform: 'translateX(0)',
+                    filter: 'blur(0px)',
+                    opacity: 1,
+                  },
+                  {
+                    transform: 'translateX(200px)',
+                    opacity: 0,
+                    filter: 'blur(1px)',
+                  },
+                ],
+                {
+                  duration: 1000,
+                }
+              );
+            }
+          });
+        },
+        {
+          threshold: 0.5,
+        }
+      );
 
-        this.observer.observe(about);
-      }
-    });
+      this.observer.observe(about);
+    }
+    // });
   }
 }
