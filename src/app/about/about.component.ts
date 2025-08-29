f62348e694a3a9ae8122158b2dbdc4b53df74ec2
Catgirl <zeroaninea@gmail.com>
2025-08-29 21:40:15 +0400

    the AboutComponent refactored

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -8,6 +8,9 @@ import {
   DOCUMENT,
   ViewChild,
   ElementRef,
+  OnDestroy,
+  AfterViewInit,
+  HostListener,
 } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 
@@ -22,43 +25,84 @@ import { AnimationsService } from '../shared/services/animations/animations.serv
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss', './about.media.component.scss'],
 })
-export class AboutComponent {
+export class AboutComponent implements AfterViewInit, OnDestroy {
   private platformId = inject(PLATFORM_ID);
   isBrowser = isPlatformBrowser(this.platformId);
 
   @ViewChild('about', { static: false }) about!: ElementRef<HTMLElement>;
 
-  private document = inject(DOCUMENT);
+  // private document = inject(DOCUMENT);
   private observer!: IntersectionObserver;
-  // private about = this.document.querySelector('#about');
+  // // private about = this.document.querySelector('#about');
   private renderer = inject(Renderer2);
 
   private animationService = inject(AnimationsService);
 
-  constructor() {
-    afterNextRender(() => {
-      console.log(this.about.nativeElement);
+  // constructor() {
+  //   afterNextRender(() => {
+  //     console.log(this.about.nativeElement);
 
-      const options =
-        window.innerWidth > 768
-          ? {
-              threshold: [0, 0.1, 0.25, 0.35, 0.5, 1],
-              rootMargin: '0px 0px -100px 0px',
-            }
-          : {
-              threshold: [0, 0.5, 1, 0.25, 0.35, 1],
-              rootMargin: '0px 0px -20px 0px',
-            };
+  //     const options =
+  //       window.innerWidth > 768
+  //         ? {
+  //             threshold: [0, 0.1, 0.25, 0.35, 0.5, 1],
+  //             rootMargin: '0px 0px -100px 0px',
+  //           }
+  //         : {
+  //             threshold: [0, 0.5, 1, 0.25, 0.35, 1],
+  //             rootMargin: '0px 0px -20px 0px',
+  //           };
 
-      if (this.about) {
-        this.observer = new IntersectionObserver((entries) => {
-          entries.forEach((entry) =>
-            this.animationService.aboutAndHtmlAnimate(entry, this.renderer)
-          );
-        }, options);
+  //     if (this.about) {
+  //       this.observer = new IntersectionObserver((entries) => {
+  //         entries.forEach((entry) =>
+  //           this.animationService.aboutAndHtmlAnimate(entry, this.renderer)
+  //         );
+  //       }, options);
 
-        this.observer.observe(this.about.nativeElement);
-      }
-    });
+  //       this.observer.observe(this.about.nativeElement);
+  //     }
+  //   });
+  // }
+
+  ngAfterViewInit(): void {
+    this.createObserver();
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    if (this.observer) {
+      this.observer.disconnect();
+    }
+    this.createObserver();
+  }
+
+  private createObserver() {
+    if (!this.about) return;
+
+    const options =
+      window.innerWidth > 768
+        ? {
+            threshold: [0, 0.1, 0.25, 0.35, 0.5, 1],
+            rootMargin: '0px 0px -100px 0px',
+          }
+        : {
+            threshold: [0, 0.25, 0.35, 0.5, 1],
+            rootMargin: '0px 0px -20px 0px',
+          };
+
+    this.observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) =>
+        this.animationService.aboutAndHtmlAnimate(entry, this.renderer)
+      );
+    }, options);
+
+    this.observer.observe(this.about.nativeElement);
+  }
+
+  ngOnDestroy(): void {
+    if (this.observer) {
+      this.observer.disconnect();
+    }
   }
 }
