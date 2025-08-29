013870e8014e4d7e0f6428593149b2f91a23ae20
Catgirl <zeroaninea@gmail.com>
2025-08-29 18:43:19 +0400

    thresholds fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,8 +41,14 @@ export class AppComponent {
             );
           },
           {
-            threshold: [0, 0.1, 0.25, 0.35, 0.5, 1],
-            rootMargin: '0px 0px -100px 0px',
+            threshold:
+              window.innerWidth <= 768
+                ? [0, 0.5, 1, 0.25, 0.35, 1]
+                : [0, 0.1, 0.25, 0.35, 0.5, 1],
+            rootMargin:
+              window.innerWidth >= 768
+                ? '0px 0px -100px 0px'
+                : '0px 0px -20px 0px',
           }
         );
 
