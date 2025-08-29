f3f9894ac54b8fcda91dc7882c452f4db6fbde01
Catgirl <zeroaninea@gmail.com>
2025-08-29 21:33:34 +0400

    the observer logic moved into the AboutComponent

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -5,12 +5,14 @@ import {
   inject,
   PLATFORM_ID,
   Renderer2,
-  DOCUMENT
+  DOCUMENT,
+  ViewChild,
+  ElementRef,
 } from '@angular/core';
-
 import { isPlatformBrowser } from '@angular/common';
 
 import { GeometricShapeComponent } from '../geometric-shape/geometric-shape.component';
+import { AnimationsService } from '../shared/services/animations/animations.service';
 
 @Component({
   selector: 'app-about',
@@ -23,4 +25,40 @@ import { GeometricShapeComponent } from '../geometric-shape/geometric-shape.comp
 export class AboutComponent {
   private platformId = inject(PLATFORM_ID);
   isBrowser = isPlatformBrowser(this.platformId);
+
+  @ViewChild('about', { static: false }) about!: ElementRef<HTMLElement>;
+
+  private document = inject(DOCUMENT);
+  private observer!: IntersectionObserver;
+  // private about = this.document.querySelector('#about');
+  private renderer = inject(Renderer2);
+
+  private animationService = inject(AnimationsService);
+
+  constructor() {
+    afterNextRender(() => {
+      console.log(this.about.nativeElement);
+
+      const options =
+        window.innerWidth > 768
+          ? {
+              threshold: [0, 0.1, 0.25, 0.35, 0.5, 1],
+              rootMargin: '0px 0px -100px 0px',
+            }
+          : {
+              threshold: [0, 0.5, 1, 0.25, 0.35, 1],
+              rootMargin: '0px 0px -20px 0px',
+            };
+
+      if (this.about) {
+        this.observer = new IntersectionObserver((entries) => {
+          entries.forEach((entry) =>
+            this.animationService.aboutAndHtmlAnimate(entry, this.renderer)
+          );
+        }, options);
+
+        this.observer.observe(this.about.nativeElement);
+      }
+    });
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,4 @@
-import {
-  Component,
-  inject,
-  Renderer2,
-  DOCUMENT,
-  afterEveryRender,
-  afterNextRender,
-} from '@angular/core';
+import { Component } from '@angular/core';
 
 // import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
@@ -23,36 +16,4 @@ import { AnimationsService } from './shared/services/animations/animations.servi
 })
 export class AppComponent {
   title = 'portfolio';
-
-  private document = inject(DOCUMENT);
-  private observer!: IntersectionObserver;
-  private about = this.document.querySelector('#about');
-  private renderer = inject(Renderer2);
-
-  private animationService = inject(AnimationsService);
-
-  constructor() {
-    afterNextRender(() => {
-      const options =
-        window.innerWidth > 768
-          ? {
-              threshold: [0, 0.1, 0.25, 0.35, 0.5, 1],
-              rootMargin: '0px 0px -100px 0px',
-            }
-          : {
-              threshold: [0, 0.5, 1, 0.25, 0.35, 1],
-              rootMargin: '0px 0px -20px 0px',
-            };
-
-      if (this.about) {
-        this.observer = new IntersectionObserver((entries) => {
-          entries.forEach((entry) =>
-            this.animationService.aboutAndHtmlAnimate(entry, this.renderer)
-          );
-        }, options);
-
-        this.observer.observe(this.about);
-      }
-    });
-  }
 }
