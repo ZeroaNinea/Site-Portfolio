65d24a1f0a9552acea44d22f2137a0cae317526e
Catgirl <zeroaninea@gmail.com>
2025-09-02 16:47:40 +0400

    PrejectComponent and ProjectItemComponent created

---
+++ src/app/project-item/project-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-item',
+  imports: [],
+  standalone: true,
+  templateUrl: './project-item.component.html',
+  styleUrl: './project-item.component.scss',
+})
+export class ProjectItemComponent {}
