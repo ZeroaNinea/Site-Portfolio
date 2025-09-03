059eae4b3317f4f98b74add249015f89ffdafdd0
Catgirl <zeroaninea@gmail.com>
2025-09-03 19:27:34 +0400

    the contacts component created

---
+++ src/app/contacts/contacts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contacts',
+  imports: [],
+  standalone: true,
+  templateUrl: './contacts.component.html',
+  styleUrl: './contacts.component.scss',
+})
+export class ContactsComponent {}
