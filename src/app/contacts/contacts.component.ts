5ef54cab23ecf9f6ce78739dce7ca8387d10a1a8
Catgirl <zeroaninea@gmail.com>
2025-09-03 21:02:48 +0400

    links added

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatRippleModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-contacts',
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatRippleModule],
   standalone: true,
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.scss', './contacts.media.component.scss'],
 })
-export class ContactsComponent {}
+export class ContactsComponent {
+  openLink(url: string) {
+    window.open(url, '_blank');
+  }
+}
