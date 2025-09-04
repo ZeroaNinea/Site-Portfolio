07f6e4b9bbea89cc26680815365766a7a0099599
Catgirl <zeroaninea@gmail.com>
2025-09-04 20:02:29 +0400

    cries

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,43 @@
+import {
+  AngularNodeAppEngine,
+  createNodeRequestHandler,
+  writeResponseToNodeResponse,
+} from '@angular/ssr/node';
+import express from 'express';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const serverDistFolder = dirname(fileURLToPath(import.meta.url));
+const browserDistFolder = resolve(serverDistFolder, '../browser');
+
+const app = express();
+const angularApp = new AngularNodeAppEngine();
+
+app.use(
+  express.static(browserDistFolder, {
+    maxAge: '1y',
+    index: false,
+    redirect: false,
+  })
+);
+
+app.use('/**', async (req, res, next) => {
+  try {
+    const response = await angularApp.handle(req);
+    if (response) {
+      writeResponseToNodeResponse(response, res);
+    } else {
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+export const handler = createNodeRequestHandler(app);
+
+/////////////////////////////////////////////////////////////////////////////
+
 // import {
 //   AngularNodeAppEngine,
 //   createNodeRequestHandler,
@@ -67,41 +107,3 @@
 
 // --- For Netlify ---
 /////////////////////////////////////////////////////////////////////////////////
-
-import {
-  AngularNodeAppEngine,
-  createNodeRequestHandler,
-  writeResponseToNodeResponse,
-} from '@angular/ssr/node';
-import express from 'express';
-import { dirname, resolve } from 'node:path';
-import { fileURLToPath } from 'node:url';
-
-const serverDistFolder = dirname(fileURLToPath(import.meta.url));
-const browserDistFolder = resolve(serverDistFolder, '../browser');
-
-const app = express();
-const angularApp = new AngularNodeAppEngine();
-
-app.use(
-  express.static(browserDistFolder, {
-    maxAge: '1y',
-    index: false,
-    redirect: false,
-  })
-);
-
-app.use('/**', async (req, res, next) => {
-  try {
-    const response = await angularApp.handle(req);
-    if (response) {
-      writeResponseToNodeResponse(response, res);
-    } else {
-      next();
-    }
-  } catch (err) {
-    next(err);
-  }
-});
-
-export const handler = createNodeRequestHandler(app);
