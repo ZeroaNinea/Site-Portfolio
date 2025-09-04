e2cd972837580257b37892a51b1508ff5fa9c904
Catgirl <zeroaninea@gmail.com>
2025-09-04 20:07:44 +0400

    cries and dies

--- src/server.ts
+++ src/server.ts
@@ -1,40 +1,28 @@
-import {
-  AngularNodeAppEngine,
-  createNodeRequestHandler,
-  writeResponseToNodeResponse,
-} from '@angular/ssr/node';
-import express from 'express';
-import { dirname, resolve } from 'node:path';
-import { fileURLToPath } from 'node:url';
+import { AngularAppEngine, createRequestHandler } from '@angular/ssr';
+import { getContext } from '@netlify/angular-runtime/context.mjs';
 
-const serverDistFolder = dirname(fileURLToPath(import.meta.url));
-const browserDistFolder = resolve(serverDistFolder, '../browser');
+const angularAppEngine = new AngularAppEngine();
 
-const app = express();
-const angularApp = new AngularNodeAppEngine();
+export async function netlifyAppEngineHandler(
+  request: Request
+): Promise<Response> {
+  const context = getContext();
 
-app.use(
-  express.static(browserDistFolder, {
-    maxAge: '1y',
-    index: false,
-    redirect: false,
-  })
-);
+  // Example API endpoints can be defined here.
+  // Uncomment and define endpoints as necessary.
+  // const pathname = new URL(request.url).pathname;
+  // if (pathname === '/api/hello') {
+  //   return Response.json({ message: 'Hello from the API' });
+  // }
 
-app.use('/**', async (req, res, next) => {
-  try {
-    const response = await angularApp.handle(req);
-    if (response) {
-      writeResponseToNodeResponse(response, res);
-    } else {
-      next();
-    }
-  } catch (err) {
-    next(err);
-  }
-});
+  const result = await angularAppEngine.handle(request, context);
+  return result || new Response('Not found', { status: 404 });
+}
 
-export const handler = createNodeRequestHandler(app);
+/**
+ * The request handler used by the Angular CLI (dev-server and during build).
+ */
+export const reqHandler = createRequestHandler(netlifyAppEngineHandler);
 
 /////////////////////////////////////////////////////////////////////////////
 
