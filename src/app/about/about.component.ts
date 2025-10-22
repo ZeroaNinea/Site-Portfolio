43dd522895f68dde6b41ad12f0271c17db344213
Catgirl <zeroaninea@gmail.com>
2025-10-22 22:35:29 +0400

    extra code removed

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -25,7 +25,7 @@ import { AnimationsService } from '../shared/services/animations/animations.serv
   styleUrls: ['./about.component.scss', './about.media.component.scss'],
   host: { ngSkipHydration: 'true' },
 })
-export class AboutComponent implements AfterViewInit, OnDestroy {
+export class AboutComponent {
   private platformId = inject(PLATFORM_ID);
   isBrowser = isPlatformBrowser(this.platformId);
 
@@ -38,54 +38,6 @@ export class AboutComponent implements AfterViewInit, OnDestroy {
   private animationService = inject(AnimationsService);
 
   showCar = false;
-
-  ngAfterViewInit(): void {
-    //   this.createObserver();
-  }
-
-  // @HostListener('window:resize')
-  // onResize() {
-  //   if (this.observer) {
-  //     this.observer.disconnect();
-  //   }
-  //   this.createObserver();
-  // }
-
-  // private createObserver() {
-  //   if (isPlatformBrowser(this.platformId)) {
-  //     if (this.about) {
-  //       const options =
-  //         window.innerWidth > 768
-  //           ? {
-  //               threshold: [0, 0.1, 0.25, 0.35, 0.5, 1],
-  //               rootMargin: '0px 0px -100px 0px',
-  //             }
-  //           : window.innerHeight >= 1080
-  //           ? {
-  //               threshold: [0, 0.1, 0.25, 0.35, 0.5, 1],
-  //               rootMargin: '0px 0px -100px 0px',
-  //             }
-  //           : {
-  //               threshold: [0, 0.25, 0.35, 0.5, 1],
-  //               rootMargin: '0px 0px -20px 0px',
-  //             };
-
-  //       this.observer = new IntersectionObserver((entries) => {
-  //         entries.forEach((entry) => {
-  //           this.animationService.aboutAndHtmlAnimate(entry, this.renderer);
-  //         });
-  //       }, options);
-
-  //       this.observer.observe(this.about.nativeElement);
-  //     }
-  //   }
-  // }
-
-  ngOnDestroy(): void {
-    //   if (this.observer) {
-    //     this.observer.disconnect();
-    //   }
-  }
 
   toggleCar() {
     console.log('toggleCar');
