366ba3b5fcd3eb6e9291439e87c74e4e0e90c142
Catgirl <zeroaninea@gmail.com>
2025-10-22 21:41:02 +0400

    remove observer logic from about

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -40,68 +40,68 @@ export class AboutComponent implements AfterViewInit, OnDestroy {
   showCar = false;
 
   ngAfterViewInit(): void {
-    this.createObserver();
+    //   this.createObserver();
   }
 
-  @HostListener('window:resize')
-  onResize() {
-    if (this.observer) {
-      this.observer.disconnect();
-    }
-    this.createObserver();
-  }
+  // @HostListener('window:resize')
+  // onResize() {
+  //   if (this.observer) {
+  //     this.observer.disconnect();
+  //   }
+  //   this.createObserver();
+  // }
 
-  private createObserver() {
-    if (isPlatformBrowser(this.platformId)) {
-      if (this.about) {
-        const options =
-          window.innerWidth > 768
-            ? {
-                threshold: [0, 0.1, 0.25, 0.35, 0.5, 1],
-                rootMargin: '0px 0px -100px 0px',
-              }
-            : window.innerHeight >= 1080
-            ? {
-                threshold: [0, 0.1, 0.25, 0.35, 0.5, 1],
-                rootMargin: '0px 0px -100px 0px',
-              }
-            : {
-                threshold: [0, 0.25, 0.35, 0.5, 1],
-                rootMargin: '0px 0px -20px 0px',
-              };
+  // private createObserver() {
+  //   if (isPlatformBrowser(this.platformId)) {
+  //     if (this.about) {
+  //       const options =
+  //         window.innerWidth > 768
+  //           ? {
+  //               threshold: [0, 0.1, 0.25, 0.35, 0.5, 1],
+  //               rootMargin: '0px 0px -100px 0px',
+  //             }
+  //           : window.innerHeight >= 1080
+  //           ? {
+  //               threshold: [0, 0.1, 0.25, 0.35, 0.5, 1],
+  //               rootMargin: '0px 0px -100px 0px',
+  //             }
+  //           : {
+  //               threshold: [0, 0.25, 0.35, 0.5, 1],
+  //               rootMargin: '0px 0px -20px 0px',
+  //             };
 
-        this.observer = new IntersectionObserver((entries) => {
-          entries.forEach((entry) => {
-            this.animationService.aboutAndHtmlAnimate(entry, this.renderer);
-          });
-        }, options);
+  //       this.observer = new IntersectionObserver((entries) => {
+  //         entries.forEach((entry) => {
+  //           this.animationService.aboutAndHtmlAnimate(entry, this.renderer);
+  //         });
+  //       }, options);
 
-        this.observer.observe(this.about.nativeElement);
-      }
-    }
-  }
+  //       this.observer.observe(this.about.nativeElement);
+  //     }
+  //   }
+  // }
 
   ngOnDestroy(): void {
-    if (this.observer) {
-      this.observer.disconnect();
-    }
+    //   if (this.observer) {
+    //     this.observer.disconnect();
+    //   }
   }
 
-  toggleCar() {
-    console.log('toggleCar');
+  // toggleCar() {
+  //   console.log('toggleCar');
 
-    this.showCar = !this.showCar;
+  //   this.showCar = !this.showCar;
 
-    if (this.showCar) {
-      setTimeout(() => {
-        this.scrollToCar();
-      }, 300);
-    } else {
-      setTimeout(() => {
-        this.scrollToAbout();
-      }, 300);
-    }
-  }
+  //   if (this.showCar) {
+  //     setTimeout(() => {
+  //       this.scrollToCar();
+  //     }, 300);
+  //   } else {
+  //     setTimeout(() => {
+  //       this.scrollToAbout();
+  //     }, 300);
+  //   }
+  // }
 
   scrollToCar() {
     this.document.querySelector('#car')?.scrollIntoView({ behavior: 'smooth' });
