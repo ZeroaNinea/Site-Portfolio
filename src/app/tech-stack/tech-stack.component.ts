b30ff87ebdd3553ed3e2a629e55b1f6327364eba
Catgirl <zeroaninea@gmail.com>
2026-02-07 13:32:14 +0400

    description implemented

--- src/app/tech-stack/tech-stack.component.ts
+++ src/app/tech-stack/tech-stack.component.ts
@@ -58,6 +58,7 @@ export class TechStackComponent implements AfterViewInit {
   ];
 
   activeTabIndex = 0;
+  expandedItemId: string | null = null;
 
   stackItems: StackItem[] = [
     {
@@ -82,7 +83,8 @@ export class TechStackComponent implements AfterViewInit {
       id: 'rxjs',
       name: 'RxJS',
       icon: 'rxjs.svg',
-      description: 'A library for reactive programming using Observables (custom data types).',
+      description:
+        'A library for reactive programming using Observables (custom data types).',
       meta: {
         tab: 'frontend',
       },
@@ -100,7 +102,8 @@ export class TechStackComponent implements AfterViewInit {
       id: 'threejs',
       name: 'Three.js',
       icon: 'threejs.svg',
-      description: 'A JavaScript library for creating and manipulating 3D models, animations, and interactions.',
+      description:
+        'A JavaScript library for creating and manipulating 3D models, animations, and interactions.',
       meta: {
         tab: 'frontend',
       },
@@ -118,7 +121,8 @@ export class TechStackComponent implements AfterViewInit {
       id: 'bootstrap',
       name: 'Bootstrap',
       icon: 'bootstrap.svg',
-      description: 'A popular CSS framework for responsive, mobile-first front-end web development.',
+      description:
+        'A popular CSS framework for responsive, mobile-first front-end web development.',
       meta: {
         tab: 'frontend',
       },
@@ -127,7 +131,8 @@ export class TechStackComponent implements AfterViewInit {
       id: 'nodejs',
       name: 'Node.js',
       icon: 'node.svg',
-      description: 'A JavaScript runtime environment that allows you to run JavaScript on the server-side.',
+      description:
+        'A JavaScript runtime environment that allows you to run JavaScript on the server-side.',
       meta: {
         tab: 'fullstack',
       },
@@ -136,7 +141,8 @@ export class TechStackComponent implements AfterViewInit {
       id: 'nextjs',
       name: 'Next.js',
       icon: 'nextjs.svg',
-      description: 'A React framework for server-rendered applications, providing features like routing, data fetching, and server-side rendering.',
+      description:
+        'A React framework for server-rendered applications, providing features like routing, data fetching, and server-side rendering.',
       meta: {
         tab: 'fullstack',
       },
@@ -145,7 +151,8 @@ export class TechStackComponent implements AfterViewInit {
       id: 'expressjs',
       name: 'Express JS',
       icon: 'expressjs.svg',
-      description: 'A minimal and flexible Node.js web application framework for building RESTful APIs.',
+      description:
+        'A minimal and flexible Node.js web application framework for building RESTful APIs.',
       meta: {
         tab: 'backend',
       },
@@ -154,7 +161,8 @@ export class TechStackComponent implements AfterViewInit {
       id: 'sequelize',
       name: 'Sequelize',
       icon: 'sequelize.svg',
-      description: 'A library for ORM (Object-Relational Mapping) in Node.js, providing a simple way to work with SQL-based databases.',
+      description:
+        'A library for ORM (Object-Relational Mapping) in Node.js, providing a simple way to work with SQL-based databases.',
       meta: {
         tab: 'backend',
       },
@@ -163,7 +171,8 @@ export class TechStackComponent implements AfterViewInit {
       id: 'mongoose',
       name: 'Mongoose',
       icon: 'mongoose.svg',
-      description: 'A library for MongoDB, providing a simple way to work with MongoDB in Node.js.',
+      description:
+        'A library for MongoDB, providing a simple way to work with MongoDB in Node.js.',
       meta: {
         tab: 'backend',
       },
@@ -181,7 +190,8 @@ export class TechStackComponent implements AfterViewInit {
       id: 'graphql',
       name: 'GraphQL',
       icon: 'graphql.svg',
-      description: 'A query language for APIs and a runtime for executing queries and mutations.',
+      description:
+        'A query language for APIs and a runtime for executing queries and mutations.',
       meta: {
         tab: 'backend',
       },
@@ -190,7 +200,8 @@ export class TechStackComponent implements AfterViewInit {
       id: 'prismaorm',
       name: 'Prisma ORM',
       icon: 'prismaorm.svg',
-      description: 'A library for ORM (Object-Relational Mapping) in Node.js, providing a simple way to work with SQL-based databases.',
+      description:
+        'A library for ORM (Object-Relational Mapping) in Node.js, providing a simple way to work with SQL-based databases.',
       meta: {
         tab: 'backend',
       },
@@ -199,7 +210,8 @@ export class TechStackComponent implements AfterViewInit {
       id: 'socketio',
       name: 'Socket.io',
       icon: 'socketio.svg',
-      description: 'A library for real-time, bidirectional communication between clients and servers. Able to limit clients to specific rooms.',
+      description:
+        'A library for real-time, bidirectional communication between clients and servers. Able to limit clients to specific rooms.',
       meta: {
         tab: 'realtime',
       },
@@ -208,7 +220,8 @@ export class TechStackComponent implements AfterViewInit {
       id: 'jwt',
       name: 'JWT',
       icon: 'jwt.svg',
-      description: 'A standard for securely transmitting information between parties as JSON objects.',
+      description:
+        'A standard for securely transmitting information between parties as JSON objects.',
       meta: {
         tab: 'backend',
       },
@@ -217,7 +230,8 @@ export class TechStackComponent implements AfterViewInit {
       id: 'auth0',
       name: 'Auth0',
       icon: 'auth0.svg',
-      description: 'A cloud-based authentication and authorization platform for web and mobile apps.',
+      description:
+        'A cloud-based authentication and authorization platform for web and mobile apps.',
       meta: {
         tab: 'backend',
       },
@@ -244,7 +258,8 @@ export class TechStackComponent implements AfterViewInit {
       id: 'docker',
       name: 'Docker',
       icon: 'docker.svg',
-      description: 'A containerization platform for building and running applications in isolated environments.',
+      description:
+        'A containerization platform for building and running applications in isolated environments.',
       meta: {
         tab: 'devops',
       },
@@ -253,7 +268,8 @@ export class TechStackComponent implements AfterViewInit {
       id: 'kubernetes',
       name: 'Kubernetes',
       icon: 'kubernetes.svg',
-      description: 'A container orchestration platform for automating the deployment, scaling, and management of containerized applications.',
+      description:
+        'A container orchestration platform for automating the deployment, scaling, and management of containerized applications.',
       meta: {
         tab: 'devops',
       },
@@ -262,7 +278,8 @@ export class TechStackComponent implements AfterViewInit {
       id: 'jenkins',
       name: 'Jenkins',
       icon: 'jenkins.svg',
-      description: 'An open source automation server for continuous integration and delivery.',
+      description:
+        'An open source automation server for continuous integration and delivery.',
       meta: {
         tab: 'devops',
       },
@@ -271,7 +288,8 @@ export class TechStackComponent implements AfterViewInit {
       id: 'cypress',
       name: 'Cypress',
       icon: 'cypress.svg',
-      description: 'A testing framework for end-to-end testing of web applications.',
+      description:
+        'A testing framework for end-to-end testing of web applications.',
       meta: {
         tab: 'testing',
       },
@@ -280,7 +298,8 @@ export class TechStackComponent implements AfterViewInit {
       id: 'jasmine',
       name: 'Jasmine',
       icon: 'jasmine.svg',
-      description: 'A behavior-driven development framework for testing JavaScript code. Used by default in Angular.',
+      description:
+        'A behavior-driven development framework for testing JavaScript code. Used by default in Angular.',
       meta: {
         tab: 'testing',
       },
@@ -289,7 +308,8 @@ export class TechStackComponent implements AfterViewInit {
       id: 'karma',
       name: 'Karma',
       icon: 'karma.svg',
-      description: 'A testing framework for JavaScript, used by default in Angular.',
+      description:
+        'A testing framework for JavaScript, used by default in Angular.',
       meta: {
         tab: 'testing',
       },
@@ -307,7 +327,8 @@ export class TechStackComponent implements AfterViewInit {
       id: 'chai',
       name: 'Chai',
       icon: 'chai.svg',
-      description: 'A BDD/TDD assertion library for node and the browser that can be delightfully paired with any javascript testing framework.',
+      description:
+        'A BDD/TDD assertion library for node and the browser that can be delightfully paired with any javascript testing framework.',
       meta: {
         tab: 'testing',
       },
@@ -316,7 +337,8 @@ export class TechStackComponent implements AfterViewInit {
       id: 'lighthouseci',
       name: 'Lighthouse CI',
       icon: 'lighthouseci.png',
-      description: 'A tool for generating and publishing Lighthouse reports for your web projects. Optimizes performance, accessibility, and SEO.',
+      description:
+        'A tool for generating and publishing Lighthouse reports for your web projects. Optimizes performance, accessibility, and SEO.',
       meta: {
         tab: 'testing',
       },
@@ -325,7 +347,8 @@ export class TechStackComponent implements AfterViewInit {
       id: 'clinicjs',
       name: 'Clinic.js',
       icon: 'clinicjs.svg',
-      description: 'An open-source performance profiling tool for Node.js applications.',
+      description:
+        'An open-source performance profiling tool for Node.js applications.',
       meta: {
         tab: 'testing',
       },
@@ -334,7 +357,8 @@ export class TechStackComponent implements AfterViewInit {
       id: 'artillery',
       name: 'Artillery',
       icon: 'artillery.svg',
-      description: 'A universal, modern, powerful & easy-to-use performance testing toolkit.',
+      description:
+        'A universal, modern, powerful & easy-to-use performance testing toolkit.',
       meta: {
         tab: 'testing',
       },
@@ -343,7 +367,8 @@ export class TechStackComponent implements AfterViewInit {
       id: 'nestjs',
       name: 'Nest.js',
       icon: 'nestjs.svg',
-      description: 'A progressive Node.js framework for building efficient, scalable, and enterprise-grade server-side applications.',
+      description:
+        'A progressive Node.js framework for building efficient, scalable, and enterprise-grade server-side applications.',
       meta: {
         tab: 'backend',
       },
@@ -352,7 +377,8 @@ export class TechStackComponent implements AfterViewInit {
       id: 'typeorm',
       name: 'TypeORM',
       icon: 'typeorm.svg',
-      description: 'An Object-Relational Mapping (ORM) tool that makes it easy to work with relational databases in Node.js and TypeScript.',
+      description:
+        'An Object-Relational Mapping (ORM) tool that makes it easy to work with relational databases in Node.js and TypeScript.',
       meta: {
         tab: 'backend',
       },
@@ -361,7 +387,8 @@ export class TechStackComponent implements AfterViewInit {
       id: 'websocket',
       name: 'WebSocket',
       icon: 'websocket.svg',
-      description: 'A communication protocol that allows real-time, bidirectional communication between a client and a server.',
+      description:
+        'A communication protocol that allows real-time, bidirectional communication between a client and a server.',
       meta: {
         tab: 'backend',
       },
@@ -370,7 +397,8 @@ export class TechStackComponent implements AfterViewInit {
       id: 'jest',
       name: 'Jest',
       icon: 'jest.svg',
-      description: 'A delightful JavaScript Testing Framework with a focus on simplicity.',
+      description:
+        'A delightful JavaScript Testing Framework with a focus on simplicity.',
       meta: {
         tab: 'testing',
       },
@@ -417,5 +445,13 @@ export class TechStackComponent implements AfterViewInit {
 
   getItemsForTab(tab: StackTab): StackItem[] {
     return this.stackItems.filter((item) => item.meta.tab === tab);
+  }
+
+  toggle(item: StackItem) {
+    this.expandedItemId = this.expandedItemId === item.id ? null : item.id;
+  }
+
+  isExpanded(item: StackItem): boolean {
+    return this.expandedItemId === item.id;
   }
 }
