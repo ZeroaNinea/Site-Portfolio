cbe2c909a0d861cc4d5f43a89a03c6e3c13abcf5
Catgirl <zeroaninea@gmail.com>
2026-05-26 20:47:07 +0400

    new links added

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -25,6 +25,26 @@ export class ContactsComponent {
       link: 'mailto:heghine.dev357@gmail.com',
       alt: 'Gmail',
     },
+    {
+      icon: './assets/contact-icons/linkedin.svg',
+      link: 'https://www.linkedin.com/in/heghine-avetisyan-650291384/',
+      alt: 'Linkedin',
+    },
+    {
+      icon: './assets/contact-icons/twitter.svg',
+      link: 'https://x.com/quiraxia65932',
+      alt: 'Twitter',
+    },
+    {
+      icon: './assets/contact-icons/medium.svg',
+      link: 'https://medium.com/@heghine.dev357',
+      alt: 'Medium',
+    },
+    {
+      icon: './assets/contact-icons/devto.svg',
+      link: 'https://dev.to/zeroaninea_8bec34a4e7d029',
+      alt: 'Dev.to',
+    },
   ];
 
   date() {
